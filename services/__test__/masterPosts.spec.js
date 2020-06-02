c79a4a7ca0c4ca8155178a774decdabbb008d7d1
nicholaspung <nicholaspungdev@gmail.com>
2020-06-01 19:50:27 -0700

    Continuing working on testing db

--- services/__test__/masterPosts.spec.js
+++ services/__test__/masterPosts.spec.js
@@ -2,47 +2,46 @@ const db = require('../../data/config');
 
 const MasterPostsService = require('../masterPosts.service');
 
-beforeEach(async () => {
-  await db('master_posts').truncate();
-});
-
 describe('masterPosts model', () => {
-  it('should insert master posts into db', async () => {
+  beforeEach(async () => {
+    await db('master_posts').truncate();
+  });
+
+  test('should insert master posts into db', async () => {
     await MasterPostsService.insert([{ id: 1 }]);
     const masterPosts = await db('master_posts');
 
     expect(masterPosts).toHaveLength(1);
   });
-  it('should get all master posts from db', async () => {
+  test('should get all master posts from db', async () => {
     await MasterPostsService.insert([{ id: 1 }]);
     const testMasterPosts = await MasterPostsService.get();
     const masterPosts = await db('master_posts');
 
     expect(testMasterPosts).toEqual(masterPosts);
   });
-  it('should delete master posts via id from db', async () => {
+  test('should delete master posts via id from db', async () => {
     await MasterPostsService.insert([{ id: 1 }]);
     await MasterPostsService.delPost(1);
     const masterPosts = await db('master_posts');
 
     expect(masterPosts).toHaveLength(0);
   });
-  it('should delete all master posts older than date from db', async () => {
-    await MasterPostsService.insert([{ id: 1, date: new Date(1) }]);
-    await MasterPostsService.insert([{ id: 2, date: new Date(2) }]);
-    await MasterPostsService.insert([{ id: 3, date: new Date() }]);
-    await MasterPostsService.del(new Date());
+  test('should delete all master posts older than date from db', async () => {
+    await MasterPostsService.insert([{ id: 1 }], new Date(1));
+    await MasterPostsService.insert([{ id: 2 }], new Date(2));
+    await MasterPostsService.insert([{ id: 3 }], new Date());
+    await MasterPostsService.del(new Date('2000-01-01'));
     const masterPosts = await db('master_posts');
 
     expect(masterPosts).toHaveLength(1);
   });
-  it('should get master posts from last queried from db', async () => {
-    await MasterPostsService.insert([{ id: 1, date: new Date(1) }]);
-    await MasterPostsService.insert([{ id: 2, date: new Date(2) }]);
-    await MasterPostsService.insert([{ id: 3, date: new Date('2000-01-01') }]);
-    const testMasterPosts = await MasterPostsService.del(new Date());
-    const masterPosts = await db('master_posts');
+  test('should get master posts from last queried from db', async () => {
+    await MasterPostsService.insert([{ id: 1 }], new Date(1));
+    await MasterPostsService.insert([{ id: 2 }], new Date(2));
+    await MasterPostsService.insert([{ id: 3 }], new Date('2000-01-01'));
+    const testMasterPosts = await MasterPostsService.getFromLastQueried(new Date('2001-01-01'));
 
-    expect(masterPosts).toEqual(testMasterPosts);
+    expect(testMasterPosts).toEqual([]);
   });
 });
---
+++ services/__test__/posts.spec.js
@@ -0,0 +1,79 @@
+const db = require('../../data/config');
+
+const PostsService = require('../posts.service');
+const TitlesService = require('../titles.service');
+
+const postSetup = (overrides = {}) => ({
+  title: 'hi',
+  comments: 'http://url',
+  url: 'http://urls',
+  reddit_id: 1,
+  user: 'test',
+  read: false,
+  created_at: new Date('2000-01-01'),
+  search_title: 'hi',
+  ...overrides,
+});
+
+describe('posts model', () => {
+  beforeEach(async () => {
+    await db('posts').truncate();
+  });
+  test('should insert a post into db', async () => {
+    await PostsService.insert(postSetup());
+    const posts = await db('posts');
+
+    expect(posts).toHaveLength(1);
+  });
+  test('should get posts for user where read is false from db', async () => {
+    const post = postSetup();
+    await PostsService.insert(postSetup());
+    const testPosts = await PostsService.get('test');
+
+    expect(testPosts).toEqual(post);
+  });
+  test('should find titles in master posts, insert into titles table, update user last_queried, and return the filtered posts', async () => {});
+  test(`should update a post's read field in db`, async () => {
+    const post = postSetup();
+    await PostsService.insert(post);
+    await PostsService.update({ user: 'test', reddit_id: 1, read: true });
+    const posts = await db('posts');
+
+    expect(posts).toEqual({
+      id: 1,
+      title: 'hi',
+      comments: 'http://url',
+      url: 'http://urls',
+      reddit_id: 1,
+      user: 'test',
+      read: true,
+      created_at: new Date('2000-01-01'),
+      search_title: 'this title',
+    });
+  });
+  test('should delete posts older than a date and where read field is true from db', async () => {
+    await PostsService.insert(postSetup());
+    await PostsService.del(new Date());
+    const posts = await db('posts');
+
+    expect(posts).toHaveLength(0);
+  });
+  test('should delete title and posts associated with title from db', async () => {
+    await PostsService.insert(postSetup());
+    await PostsService.insert(postSetup({ search_title: 'bye', reddit_id: 2 }));
+    await TitlesService.insert({ user: 'test', title: 'hi' });
+    await PostsService.delTitleAndPosts({ user: 'test', title: 'hi' });
+    const posts = await db('posts');
+    const titles = await db('titles');
+
+    expect(posts).toHaveLength(1);
+    expect(titles).toHaveLength(0);
+  });
+  test('should delete a post by id', async () => {
+    await PostsService.insert(postSetup());
+    await PostsService.delId(1);
+    const posts = await db('posts');
+
+    expect(posts).toHaveLength(0);
+  });
+});
