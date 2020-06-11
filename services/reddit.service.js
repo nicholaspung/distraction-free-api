6e27e4fcbc44e18eb56a0277e00da93c68d5a7ff
nicholaspung <nicholaspungdev@gmail.com>
2020-06-10 20:58:12 -0700

    Almost getting Reddit auth to work

--- services/reddit.service.js
+++ services/reddit.service.js
@@ -1,18 +1,21 @@
 const axios = require('axios');
-const { URLSearchParams } = require('url');
 require('dotenv').config();
 
 const REDDIT_ACCESS_TOKEN_URL = 'https://www.reddit.com/api/v1/access_token';
-const APP_ONLY_GRANT_TYPE = 'https://oauth.reddit.com/grants/installed_client';
 
 const get = async () => {
-  const { REDDIT_CLIENT_ID } = process.env;
-
-  const params = new URLSearchParams();
-  params.append('grant_type', APP_ONLY_GRANT_TYPE);
-  params.append('device_id', 'DO_NOT_TRACK_THIS_DEVICE');
-
-  const tokenData = await axios.post(REDDIT_ACCESS_TOKEN_URL);
+  const tokenData = await axios.post(REDDIT_ACCESS_TOKEN_URL, {
+    body: {
+      grant_type: 'password',
+      username: process.env.REDDIT_USERNAME,
+      password: process.env.REDDIT_PASSWORD,
+    },
+    auth: {
+      username: process.env.REDDIT_CLIENT_ID,
+      password: process.env.REDDIT_CLIENT_SECRET,
+    },
+  });
+  console.log(tokenData);
 };
 
 module.exports = {
